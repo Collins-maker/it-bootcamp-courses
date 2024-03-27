2ccbf411d0aeae456552f9eb1858bd35d4d112c9
collins-maker <muchuicollins56@gmail.com>
2024-03-27 08:59:58 +0300

    improved hero sections for the instructors

--- src/pages/pages/instructors/InstructorsPage.js
+++ src/pages/pages/instructors/InstructorsPage.js
@@ -1,12 +1,17 @@
 import React from "react";
 import Hero from "../../../components/hero/Hero";
 import Instructors from "../../../components/instructors/Instructors";
+import heroInstructor from "../../../images/heroInstructor.jpg"
 
 
 const InstructorsPage = () =>{
     return (
         <div className="Instructorspage">
-            <Hero />
+            <Hero
+        title="MEET OUR INSTRUCTORS"
+        description="Throughout your bootcamp, you will be backed by a professional team dedicated to helping you accomplish your goals."
+        heroImage={heroInstructor}
+      />
             <Instructors />
         </div>
     );
